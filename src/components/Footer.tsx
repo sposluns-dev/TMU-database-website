d42659734850c550c994e99e4087e057304352f6
simonp221 <213841365+sposluns-dev@users.noreply.github.com>
2026-08-07 15:17:45 -0400

    _

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,7 @@ export const Footer = () => (
         <div className="container">
             <p>Jewish Identity in Canadian Law: A Database</p>
             <p className="copyright">
-                © {new Date().getFullYear()} JICL Database. Decision text sourced from{" "}
-                <a href="https://a2aj.ca/" target="_blank" rel="noopener noreferrer">
-                    A2AJ
-                </a>{" "}
-                and CanLII.
+                © {new Date().getFullYear()} JICL Database
             </p>
         </div>
     </footer>
